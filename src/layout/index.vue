efa8111e354a8a5a91fc1432d5b8b86a1285aabc
zhanghuihuang <zhanghuihuang@58.com>
2021-01-25 15:57:42 +0800

    添加node读取文件

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -18,40 +18,79 @@
       </el-row>
     </el-aside>
     <el-main>
-
+      <el-input
+          type="textarea"
+          :rows="100"
+          placeholder="请输入Hosts文件内容"
+          v-model="currentHostContent">
+      </el-input>
     </el-main>
   </el-container>
 </template>
 
 <script>
+const defaultHostId = "1"
+const fs = require("fs")
+const path = require('path')
 
 export default {
   name: "Layout",
   data() {
     return {
-      currentHostId: "1",
+      currentHostId: defaultHostId,
+      currentHostContent: "",
       hostList: [
         {
           id: 1,
           name: "默认",
-          content: "",
+          content: "默认",
           switchFlag: true
         },
         {
           id: 2,
           name: "物流商品",
-          content: "",
+          content: "物流商品",
+          switchFlag: false
+        },
+        {
+          id: 3,
+          name: "同城APP",
+          content: "同城APP",
           switchFlag: false
         }
       ]
     };
   },
+  mounted() {
+    for (let host of this.hostList) {
+      if (host.id == this.currentHostId) {
+        this.currentHostContent = host.content
+        let basedir = path.resolve("./")
+        console.log(basedir)
+        var data = fs.readFileSync('D:\\username.name');
+        console.log("同步读取: " + data.toString());
+      }
+    }
+  },
   methods: {
     handleChange(newValue, id) {
       if (newValue) {
         //如果开,则关闭所有其他的host
+        for (let host of this.hostList) {
+          if (host.id != id) {
+            host.switchFlag = false
+          } else {
+            this.currentHostContent = host.content
+          }
+        }
       } else {
-        //如果关,判断当前
+        //如果关,则设置默认为当前host
+        for (let host of this.hostList) {
+          if (host.id == defaultHostId) {
+            host.switchFlag = true
+            this.currentHostContent = host.content
+          }
+        }
       }
     }
   }
